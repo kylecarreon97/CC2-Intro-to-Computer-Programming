970d7ed9b371b1ad6166cb9d1949101c4688717a
kylecarreon97 <44014562+kylecarreon97@users.noreply.github.com>
2018-12-01 09:08:35 +0800

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,12 @@
+public class HelloWorld2
+{
+   public static void main(String[] args) {
+
+       System.out.println("Name: Kyle S. Carreon");
+       System.out.println("Birthday: October 15, 2018");
+       System.out.println("Age: 21 yrs. Old");
+       System.out.println("Sex: Male");
+       System.out.println("Pi: 3.1416");
+    
+   }
+}
